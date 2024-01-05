45390497ce05b2b4d186664d8f9e1963f4d6a578
Axel Verdasca <axel.verdasca@numericable.fr>
2024-01-05 12:50:55 +0100

    Filtre + login

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -1,8 +1,8 @@
 let loginform = document.getElementById("loginform");
 
 
-function redirectionAccueil(){
-    document.location.href = "index.htlm"
+ function redirectionAccueil(){
+    document.location.href = "./index.htlm"
 }
 
 loginform.addEventListener("submit", async function(event){
@@ -10,17 +10,28 @@ loginform.addEventListener("submit", async function(event){
 
     const emailValue = document.getElementById("email").value;
     const passwordValue = document.getElementById("password").value;
-
+    const logs = {emailValue,passwordValue};
+    
+    // Envoi des logs
     const response = await fetch(" http://localhost:5678/api/users/login",{
         method:"POST",
         headers: {"Content-type" : "application/json",},
-        body: JSON.stringify(emailValue, passwordValue),
+        body: JSON.stringify(logs),
     })
 
-    if (response.ok){
-        const data = await response.json()
+    // Retour des réponses
+    const retourReponse = await response.json()
+    const token = retourReponse.token
+    const etat = response.ok
 
-        localStorage.setItem("Token",data.token)
-    
+    // Redirection vers page d'acceuil si les logs sont bon
+    if(etat === true){
+        localStorage.setItem("token",token)
+        localStorage.setItem("etat",etat)
+        redirectionAccueil()
+    } else{
+        localStorage.setItem("etat", etat)
+        const erreur = document.querySelector(".error__log")
+        erreur.innerText = "email ou mot de passe incorrect !"
     }
 })
--- FrontEnd/assets/JS/script.js
+++ FrontEnd/assets/JS/script.js
@@ -1,56 +1,77 @@
-// Récupération des traveaux 
-
-const responseworks = await fetch("http://localhost:5678/api/works");
-const works = await responseworks.json()
-
-
-// Récupération des catégories 
-
-const responsecategories = await fetch("http://localhost:5678/api/categories")
-const categories = await responsecategories.json()
-
-// Gallerie 
-
-function gallerie(works){
-    for(let i = 0; i < works.lenght; i++){
-        const work = works[i];
-
-        const sectiongallerie = document.querySelector(".gallery")
-
-        // Création des balises figure
-        const gallerieElement = document.createElement("figure")
-        gallerieElement.dataset.id = work.id
-
-        // Création des balise img
-        const imageElement = document.createElement("img")
-        imageElement.src = work.imageUrl
-        imageElement.alt = work.title
-
-        // Création des balises figcaption
-        const figcaptionElement = document.createElement("figcaption")
-        figcaptionElement.innerText = work.title
-
-        // Rattachement 
-        sectiongallerie.appendChild(gallerieElement)
-        gallerieElement.appendChild(imageElement)
-        gallerieElement.appendChild(figcaptionElement)
+// Génaration gallerie + catégorie
+async function gallerie(filterWorks = null) {
+    // Récupération des travaux
+    const responseWorks = await fetch("http://localhost:5678/api/works");
+    const works = await responseWorks.json();
+  
+    // Récupération des catégories
+    const responseCategories = await fetch("http://localhost:5678/api/categories");
+    const categories = await responseCategories.json();
+  
+    // Bouton Tous
+    const Tous = { "id": 0, "name": "Tous" };
+    categories.unshift(Tous);
+  
+    // Catégories
+    const filtrecategories = document.querySelector(".filtre__categories");
+    filtrecategories.innerHTML = ""; // Réinitialise la liste des boutons
+  
+    for (let i = 0; i < categories.length; i++) {
+      const categorie = categories[i];
+  
+      const bouttoncategories = document.createElement("button");
+      bouttoncategories.innerText = categorie.name;
+  
+      filtrecategories.appendChild(bouttoncategories);
+  
+      // Filtrage
+      
+      bouttoncategories.addEventListener("click", function() {
+        if (categorie.id === 0) {
+          document.querySelector(".gallery").innerHTML = "";
+         gallerie();
+        } else {
+          const filtersWorks = works.filter(function(work) {
+            return work.categoriesId === categorie.id; 
+          });
+          document.querySelector(".gallery").innerHTML = "";
+         gallerie(filtersWorks);
+        }
+      });
     }
+  
+    // Gallerie
+    const sectiongallerie = document.querySelector(".gallery");
+    sectiongallerie.innerHTML = ""; // Réinitialise la galerie
+  
+    const worksToDisplay = filterWorks || works; // Utilise les travaux filtrés s'ils existent
+  
+    for (let i = 0; i < worksToDisplay.length; i++) {
+      const work = worksToDisplay[i];
+  
+      // Création des balises figure
+      const gallerieElement = document.createElement("figure");
+      gallerieElement.dataset.id = work.id;
+  
+      // Création des balise img
+      const imageElement = document.createElement("img");
+      imageElement.src = work.imageUrl;
+      imageElement.alt = work.title;
+  
+      // Création des balises figcaption
+      const figcaptionElement = document.createElement("figcaption");
+      figcaptionElement.innerText = work.title;
+  
+      // Rattachement
+      sectiongallerie.appendChild(gallerieElement);
+      gallerieElement.appendChild(imageElement);
+      gallerieElement.appendChild(figcaptionElement);
 
-   
-}
-
-gallerie(works)
-
-
-// Catégories
-for(let i = 0; i < categories.lenght; i++){
-    const categorie = categories[i]
-
-    const filtrecategories = document.querySelector(".filtre__categories")
-
-    const bouttoncategories = document.createElement("button")
-    bouttoncategories.innerText = categorie.name
+    }
+  }
+  
+  gallerie();
 
 
-    filtrecategories.appendChild(bouttoncategories)
-}
+
+
